fb87d11d2202d3fd90db23bb8def78cacf4d7292
Azeemat Raji <azeematjumoke@gmail.com>
2024-11-25 05:07:34 -0500

    modified terraform directory

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "company_domain_name" {
+  default = "azeemahraji.me"
+}
+
+variable "company_name" {
+  default = "azeemat"
+}
+
+variable "profile" {}
+
+variable "stage" {}
+
+variable "region" {}
